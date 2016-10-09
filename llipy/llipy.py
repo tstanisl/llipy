7e429a004f50e7301e6a3522c7d470e047b8cf9b
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2016-10-09 13:59:00 +0200

    initial

---
+++ llipy/llipy.py
@@ -0,0 +1,29 @@
+"Parser for LLVM IR in human readable form (.ll) files."
+
+import abc
+
+from pyparsing import (
+    delimitedList,
+    Empty,
+    Keyword,
+    MatchFirst,
+    Regex,
+)
+
+def keywords(keywords):
+    """Helper to quickly define a set of alternative Keywords.
+     Keywords are matched using MatchFirst."""
+    return MatchFirst(Keyword(word) for word in keywords.split())
+
+def commalist(entry):
+    "Helper to define a comma separated list. The list can be empty."
+    return delimitedList(entry) | Empty()
+
+class Node(metaclass=abc.ABCMeta):
+    "Base class for all llipy nodes"
+    @abc.abstractclassmethod
+    def parser(cls):
+        "Returns a PyParsing parser for given class"
+
+NUMBER = Regex(r'-?\d+').setParseAction(lambda tok: int(tok[0]))
+
