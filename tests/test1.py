c294ee25b94fd21a71375c428e9534ff45d6c938
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2016-10-10 16:46:36 +0200

    tests: test1: add tests for ArrayType

--- tests/test1.py
+++ tests/test1.py
@@ -22,5 +22,31 @@ class TestScalarTypes(unittest.TestCase):
                 pval = self.parser.parseString(txt)[0]
                 self.assertEqual(pval, obj)
 
+class TestArrayType(unittest.TestCase):
+    parser = ll.ArrayType.parser()
+    def test(self):
+        tests = [
+            ('[4 x i1]', ll.INT1, (4,)),
+            ('[50 x [4 x i32]]', ll.INT32, (50, 4)),
+            ('[1 x [1 x [1 x i64]]]', ll.INT64, (1, 1, 1)),
+        ]
+        def analyze(arr):
+            "Extracts base type and dimensions from ArrayType"
+            if isinstance(arr, ll.ArrayType):
+                slots = arr.slots()
+                etype, dim = analyze(arr.etype())
+                return etype, (slots,) + dim
+            return arr, ()
+
+        for txt, etype, dim in tests:
+            with self.subTest(val=txt):
+                pval = self.parser.parseString(txt)[0]
+                self.assertTrue(isinstance(pval, ll.ArrayType))
+
+                petype, pdim = analyze(pval)
+
+                self.assertEqual(etype, petype)
+                self.assertEqual(dim, pdim)
+
 if __name__ == '__main__':
     unittest.main()
