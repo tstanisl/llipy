b0fb84752aa7299c66500f621865b7106797385a
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2016-10-12 12:10:10 +0200

    tests: test1: remove ll

--- tests/test1.py
+++ tests/test1.py
@@ -4,18 +4,25 @@ import unittest
 
 import pyparsing
 
-import llipy.llipy as ll
+from llipy.llipy import (
+    ArrayType,
+    INT1, INT8, INT16, INT32, INT64,
+    ScalarType,
+    StructType,
+    Type,
+    VOID,
+)
 
 class TestScalarTypes(unittest.TestCase):
-    parser = ll.ScalarType.parser()
+    parser = ScalarType.parser()
     def test(self):
         tests = [
-            ('void', ll.VOID),
-            ('i1', ll.INT1),
-            ('i8', ll.INT8),
-            ('i16', ll.INT16),
-            ('i32', ll.INT32),
-            ('i64', ll.INT64),
+            ('void', VOID),
+            ('i1', INT1),
+            ('i8', INT8),
+            ('i16', INT16),
+            ('i32', INT32),
+            ('i64', INT64),
         ]
         for txt, obj in tests:
             with self.subTest(val=txt):
@@ -23,16 +30,16 @@ class TestScalarTypes(unittest.TestCase):
                 self.assertEqual(pval, obj)
 
 class TestArrayType(unittest.TestCase):
-    parser = ll.ArrayType.parser()
+    parser = ArrayType.parser()
     def test(self):
         tests = [
-            ('[4 x i1]', ll.INT1, (4,)),
-            ('[50 x [4 x i32]]', ll.INT32, (50, 4)),
-            ('[1 x [1 x [1 x i64]]]', ll.INT64, (1, 1, 1)),
+            ('[4 x i1]', INT1, (4,)),
+            ('[50 x [4 x i32]]', INT32, (50, 4)),
+            ('[1 x [1 x [1 x i64]]]', INT64, (1, 1, 1)),
         ]
         def analyze(arr):
             "Extracts base type and dimensions from ArrayType"
-            if isinstance(arr, ll.ArrayType):
+            if isinstance(arr, ArrayType):
                 slots = arr.slots()
                 etype, dim = analyze(arr.etype())
                 return etype, (slots,) + dim
@@ -41,7 +48,7 @@ class TestArrayType(unittest.TestCase):
         for txt, etype, dim in tests:
             with self.subTest(val=txt):
                 pval = self.parser.parseString(txt)[0]
-                self.assertTrue(isinstance(pval, ll.ArrayType))
+                self.assertTrue(isinstance(pval, ArrayType))
 
                 petype, pdim = analyze(pval)
 
@@ -49,17 +56,17 @@ class TestArrayType(unittest.TestCase):
                 self.assertEqual(dim, pdim)
 
 class TestStructType(unittest.TestCase):
-    parser = ll.StructType.parser()
+    parser = StructType.parser()
     def test(self):
         tests = [
             ('{}', (), 0),
-            ('{i1}', (ll.INT1,), 1),
-            ('{i8, i16}', (ll.INT8, ll.INT16), 3),
+            ('{i1}', (INT1,), 1),
+            ('{i8, i16}', (INT8, INT16), 3),
         ]
         for txt, etypes, size in tests:
             with self.subTest(val=txt):
                 pval = self.parser.parseString(txt)[0]
-                self.assertTrue(isinstance(pval, ll.StructType))
+                self.assertTrue(isinstance(pval, StructType))
                 self.assertEqual(pval.slots(), len(etypes))
 
                 for idx, etype in enumerate(etypes):
